3344e4d4d29a8cb26e004c887567847d3c5f9295
Zakaria Aaichaou <125461010+Z-Sitawi@users.noreply.github.com>
2024-01-31 11:21:24 +0100

    fix 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,10 +11,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	if (!parent)
-	{
+	if (parent == NULL)
 		return (NULL);
-	}
 
 	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
